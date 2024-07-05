fdb087f3920c7345cacb70c518a4621898231889
inline-arc <atharvmore20@gmail.com>
2024-07-05 17:15:34 +0530

    config setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
-  networks:{
-    hardhad: {
-      forking: {
-        url: "https://api-cardona-zkevm.polygonscan.com/api",
+solidity: "0.8.24",
+paths: {
+    artifacts: "./src",
+},
+networks: {
+    zkEVM: {
+    url: `https://rpc.cardona.zkevm-rpc.com`,
+    accounts: [process.env.PRIVATE_KEY],
     },
-  }
-}};
+},
+};
